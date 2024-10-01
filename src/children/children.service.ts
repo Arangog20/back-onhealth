aa75266ad49b5d79291f19b3a0b0716758c68624
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-10-01 12:18:37 -0500

    se crea entidad y dto para children

--- src/children/children.service.ts
+++ src/children/children.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChildDto } from './dto/create-child.dto';
 import { UpdateChildDto } from './dto/update-child.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Child } from './entities/child.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ChildrenService {
-  create(createChildDto: CreateChildDto) {
+  constructor(
+    @InjectModel(Child.name)
+    private readonly childModel: Model<Child>
+  ){}
+
+
+ async create(createChildDto: CreateChildDto): promise<Child> {
+  const existingChild = await this.childModel.findOne({})
     return 'This action adds a new child';
   }
 
