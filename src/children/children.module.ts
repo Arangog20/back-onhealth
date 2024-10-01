1175dc5ba8611e982fb8d19e06ecd1cfaf7a33a0
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-10-01 14:21:48 -0500

    implementacion de imports con forfeature parala entidad

--- src/children/children.module.ts
+++ src/children/children.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ChildrenService } from './children.service';
 import { ChildrenController } from './children.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Child, ChildSchema } from './entities/child.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: Child.name, schema: ChildSchema}
+    ])
+  ],
   controllers: [ChildrenController],
   providers: [ChildrenService],
 })
